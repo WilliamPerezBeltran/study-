d38592545377b362d1bc4bf28e7a26bd7a0de904
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-08-12 23:15:33 -0500

    add files

--- python/2025/00_MIT_courses/00-A-Gentle-Introduction-to-Programming-Using-Python/notas/list.py
+++ python/2025/00_MIT_courses/00-A-Gentle-Introduction-to-Programming-Using-Python/notas/list.py
@@ -22,3 +22,12 @@ to help sort this out!
 http://www.pythontutor.com/
 
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
+plt.show()  
